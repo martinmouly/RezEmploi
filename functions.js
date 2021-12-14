e65c0216ec97820539d6d2c567a81d7c546002c7
Pierregvx <pierregue75@gmail.com>
2021-12-14 17:59:20 +0100

    js implementation

---
+++ functions.js
@@ -0,0 +1,7 @@
+function proutfunction() {
+    console.log("test")
+  document.getElementById("contact").innerHTML = "Prout";
+}
+function getProfile(){
+    document.location.href='profile.html';
+}
