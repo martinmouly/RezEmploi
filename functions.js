7aad0dd77a68cbbab5663870d453bd75f8a84bf7
Pierregvx <pierregue75@gmail.com>
2021-12-30 11:57:44 +0100

    side dans une classe pour simplifier

--- functions.js
+++ functions.js
@@ -17,3 +17,23 @@ function foo (txt) {
    }
  });
 }
+function UpdateStatus(){
+
+  //make an ajax call and get status value using the same 'id'
+  var var1= document.getElementById("status").value;
+  $.ajax({
+  
+          type:"GET",//or POST
+          url:'profile.php',
+                             //  (or whatever your url is)
+          data:{'id':0},
+          //can send multipledata like {data1:var1,data2:var2,data3:var3
+          //can use dataType:'text/html' or 'json' if response type expected 
+          success:function(responsedata){
+                 // process on data
+                 alert("got response as "+"'"+responsedata+"'");
+  
+          }
+       })
+  
+  }
--- profile.php
+++ profile.php
@@ -7,42 +7,13 @@
     <link rel = "stylesheet" href = "stylehome.css">
 </head>
 <body>
-    <nav>
-        <div class = "nav-left">
-            <img src = "images/logo.png" class = "logo"onclick="location.href='index.html'">
-            <ul>
-                <li><img src = "images/notification.png" ></li>
-                <li><img src = "images/inbox.png" onclick="location.href='messagerie.html'">></li>
-                <li><img src = "images/job.png" onclick="location.href='jobs.html'"></li>
-              </ul>
-        
-        </div>
-        <div class = "nav-right">
-            <div class ="search-box">
-                <img src = "images/search.png">
-                <input type ="text" placeholder = "Search">
-            </div>
-            <div class = "nav-user-icon">
-                <a onclick="getProfile()">
-                    <img src = "images/profile-pic.png" >
-                </a>
-
-                
-
-            </div>
-        </div>
-    </nav>
+<?php
+    include("side.php");
+    $side = new Side();
+    $side->navbar();
+    ?>
     <div class="container">
-        <div class="left-sidebar" id = "cprofile">
-            <div class="imp-links">
-                <a href="#"><img src = "images/news.png">News</a>
-                <a href="#"><img src = "images/friends.png" onclick="location.href='listppl.html'">Friends</a>
-                <a href="#"><img src = "images/group.png">Group</a>
-                <a href="#"><img src = "images/watch.png">watch</a>
-
-            </div>
-
-        </div>
+    <?php $side->left();?>
         <div class="main-content"id = "centerprofile">
             <?php
             include("Person.php");
@@ -67,65 +38,9 @@
             $perso = $profiles[$idSelect];
             //display all in the Person class
             $perso->getDescription();
-            ?>
-            
+            ?>  
         </div>
-
-
-
-
-
-
-
-
-
-        <div class="right-sidebar" id = "cprofile">
-            <div class="sidebar-title">
-                <h4>Events</h4>
-                <a href = "#">See All</a>
-            </div>
-
-            <div class="event">
-                <div class="left-event">
-                    <h3>18</h3>
-                    <span>March</span>
-                </div>
-                <div class="rightleft-event">
-                    <h4>Event 1 </h4>
-                    <p>description</p>
-                    <a href="#">More info</a>
-                </div>
-            </div>
-            <div class="sidebar-title">
-                <h4>Publicité</h4>
-                <a href = "#">close</a>
-            </div>
-            <img src="images/advertisement.png" class="sidebar-ads">
-            <div class="sidebar-title">
-                <h4>Conversation</h4>
-                <a href = "#">hide chat</a>
-            </div>
-
-            <div class="online-list">
-                <div class="online">
-                    <img src="images/member-1.png" >
-                </div>
-                <p>Prenom Nom</p>
-            </div>
-            <div class="online-list">
-                <div class="online">
-                    <img src="images/member-2.png" >
-                </div>
-                <p>Prenom2 Nom2</p>
-            </div>
-            <div class="online-list">
-                <div class="online">
-                    <img src="images/member-3.png" >
-                </div>
-                <p>Prenom3 Nom3</p>
-            </div>
-
-        </div>
+        <?php $side->left();?>
         
     </div>
 </body>
