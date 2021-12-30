e7f0543b6cbe172b43e7c41323a976b08279420c
Martin Mouly <martin@mouly.name>
2021-12-30 19:47:48 +0100

    Add files via upload

---
+++ register.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+ 
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Login and Registration form example</title>
+ 
+<link rel="stylesheet" type="text/css" href="login.css">
+ 
+</head>
+ 
+<body>
+ 
+<div id="container">
+<form method="post" id="flogin">
+<?php
+$conn = mysqli_connect('localhost','root','password','dbtest');
+if(isset($_POST['submit']))
+{		
+    $username = $_POST['user'];
+    $pwd = $_POST['pass'];
+
+    $insert = mysqli_query($conn,"INSERT INTO `users`(`pseudo`, `pwd`) VALUES ('$username','$pwd')");
+
+    if(!$insert)
+    {
+        echo mysqli_error();
+    }
+    else
+    {
+        echo "Records added successfully.";
+    }
+
+}
+?>
+<div class="border-box">
+<h2>New account</h2>
+<input type="text" name="user" placeholder="Enter Username" id="uname"><br/>
+ 
+<input type="password" name="pass" placeholder="Enter Password" id="upass"><br/>
+ 
+<button type="submit" value="Submit" name="submit">Register</button>
+
+<a href="index.php">Already have an account? Sign in!</a>
+</div>
+ 
+</form>
+</div>
+ 
+</body>
+</html>
