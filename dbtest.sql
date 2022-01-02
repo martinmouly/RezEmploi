1cdcb1c8913b303bcd7f59240b4b963f1f94da09
Martin Mouly <martin@mouly.name>
2022-01-02 16:59:02 +0100

    Update dbtest.sql

--- dbtest.sql
+++ dbtest.sql
@@ -2,32 +2,37 @@ create database if not exists dbtest;
 use dbtest;
 
 -- TABLES 
+
+drop table if exists post;
+
 drop table if exists users;
 drop table if exists post;
-drop table if exists cv;
+drop table if exists experience;
 
-
--- FOURNISSEUR ///////////////////////////////////////
-create table cv(
-id int primary key not null auto_increment,
-formation varchar(1000),
-experience varchar(1000),
-extra varchar(1000)
-);
 create table users(
 id int primary key not null auto_increment,
 pseudo varchar(50),
 pwd varchar(50),
 nom varchar(50),
 prenom varchar(50),
-photo varchar(50),
-id_cv int,
-FOREIGN KEY (id_cv) REFERENCES cv(id)
+photo varchar(50)
 );
+
+create table experience(
+id int primary key not null auto_increment,
+formation varchar(1000),
+experience varchar(1000),
+extra varchar(1000)
+);
+
 create table post(
 id int primary key not null auto_increment,
 texte varchar(50),
 img varchar(50),
 likes int,
-id_user,
+id_user int,
 FOREIGN KEY (id_user) REFERENCES users(id)
+);
+
+
+
